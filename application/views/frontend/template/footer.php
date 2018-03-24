2df9c2c227607097ebfd1e04440f3bafdf4ae4eb
Unknown <16171281@itculiacan.edu.mx>
2018-03-24 10:56:14 -0700

    renamed main_frontend

--- application/views/frontend/template/footer.php
+++ application/views/frontend/template/footer.php
@@ -2,6 +2,6 @@
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src=<? echo base_url()."assets/js/materialize.min.js"?>></script>
       <script type="text/javascript"  src=<?php echo base_url()."assets/js/particles.js";?>></script>
-      <script type="text/javascript"  src=<?php echo base_url()."assets/js/main.js";?>></script>
+      <script type="text/javascript"  src=<?php echo base_url()."assets/js/main_frontend.js";?>></script>
     </body>
   </html>
