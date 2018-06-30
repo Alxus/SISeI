e14f2b65491a5c154df3cb5fdabc18c8b9cd6be8
Unknown <davidmv1295@gmail.com>
2018-06-30 14:49:36 -0600

    Comprobante

--- application/controllers/Ventas_controller.php
+++ application/controllers/Ventas_controller.php
@@ -24,8 +24,12 @@ class Ventas_controller extends CI_Controller {
 	}
 
 	public function Abono(){
+		$data['facebook_id']=$this->input->post('fb');
+		$data['email']=$this->input->post('email');
+		$data['carnet_id']=$this->input->post('carnet');
+		$asistente=$this->Asistentes_model->getbByEmail_OR_FbId($data);
 		//El usuario no se encuentra en nuestra BD
-		if($this->input->post('fb')=='' && $this->Asistentes_model->getbyid($this->input->post('email'))==null){
+		if($asistente==null){
 			$asistente['nombre_real']=$this->input->post('nombre');
 			$asistente['apellido_real']=$this->input->post('apellido');
 			$asistente['no_control']=$this->input->post('noControl');
@@ -42,26 +46,21 @@ class Ventas_controller extends CI_Controller {
 				$abono['carnet_id']=$this->input->post('carnet');
 				$abono['debe']=$precio-$this->input->post('abono');
 				$abono['estado']=($abono['debe']==0)?'PAGADO':'APARTADO';
-				if($abono['estado']=='PAGADO'){
-					if(($this->Pagos_model->getPagados()[0]['total']+$this->Asistentes_model->getPagados()[0]['total'])<50)
-						$asistente['pro']=true;
-				}
-				if($this->Asistentes_model->abono_Asistente($abono)){
-					$this->pdf->AliasNbPages();
-					$this->pdf->AddPage();
-					$this->pdf->SetFont('Arial','B',12);
-					$this->pdf->MultiCell(0,10,'Comprobante de pago');
-					$this->pdf->SetFont('Arial','B',10);
-					$this->pdf->Ln();
-					$this->pdf->SetFont('Arial','',10);
-					$this->pdf->MultiCell(0,10,$asistente['nombre_real']);
-					$this->pdf->MultiCell(0,10,$asistente['apellido_real']);
-					$this->pdf->MultiCell(0,10,$asistente['email']);
-					$this->pdf->MultiCell(0,10,$asistente['password']);
-					$this->pdf->Output('comprobante_pago.pdf', 'I');
-				}
 			}
 		}
+		else{
+			$abono['asistente_id']=$asistente['id'];
+			$abono['carnet_id']=$asistente['carnet_id'];
+			$abono['debe']=$asistente['debe']-$this->input->post('abono');
+			$abono['estado']=($abono['debe']==0)?'PAGADO':'APARTADO';
+		}
+		if($abono['estado']=='PAGADO' && ($this->Pagos_model->getPagados()[0]['total']+$this->Asistentes_model->getPagados()[0]['total'])<50){
+			$asistente['pro']=true;
+		}
+		print_r($abono);
+		$this->Asistentes_model->abono_asistente($abono);
+		$asistente['comprobante']=$abono;
+		$this->printComprobante($asistente);
 	}
 
 	public function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'){
@@ -71,6 +70,21 @@ class Ventas_controller extends CI_Controller {
 			$pieces []= $keyspace[random_int(0, $max)];
 		}
 		return implode('', $pieces);
+	}
+
+	public function printComprobante($asistente){
+		$this->pdf->AliasNbPages();
+		$this->pdf->AddPage();
+		$this->pdf->SetFont('Arial','B',12);
+		$this->pdf->MultiCell(0,10,'Comprobante de pago');
+		$this->pdf->SetFont('Arial','B',10);
+		$this->pdf->Ln();
+		$this->pdf->SetFont('Arial','',10);
+		$this->pdf->MultiCell(0,10,$asistente['nombre_real']);
+		$this->pdf->MultiCell(0,10,$asistente['apellido_real']);
+		$this->pdf->MultiCell(0,10,$asistente['email']);
+		$this->pdf->MultiCell(0,10,$asistente['password']);
+		$this->pdf->Output('comprobante_pago.pdf', 'I');
 	}
 
 }
--- application/models/Asistentes_model.php
+++ application/models/Asistentes_model.php
@@ -43,7 +43,13 @@ class Asistentes_model extends CI_Model{
     }
 
     public function abono_asistente($data){
-        return $this->db->insert('asistente_carnet',$data);
+        if($this->db->where('asistente_id',$data['asistente_id'])->where('carnet_id',$data['carnet_id'])->get('asistente_carnet')==null){
+            return $this->db->insert('asistente_carnet',$data);
+        }
+        else{
+            $data['updated_at']=date('Y-d-m H:i:s');
+            return $this->db->where('asistente_id',$data['asistente_id'])->where('carnet_id',$data['carnet_id'])->update('asistente_carnet',$data);
+        }
     }
 
     public function getPagados(){
@@ -51,7 +57,23 @@ class Asistentes_model extends CI_Model{
         return $this->db->get()->result_array();
     }
 
-    public function getbyemail($email){
-        return $this->db->where('email',$email)->get('asistente')->result_array();
+    public function getbByEmail_OR_FbId($data){
+        if($data['facebook_id']!=null){
+            $this->db->select('a.*, ac.*');
+            $this->db->from('asistente as a');
+            $this->db->join('asistente_carnet as ac','a.id=ac.asistente_id');
+            $this->db->join('carnet as c','c.id=ac.carnet_id');
+            $this->db->where('a.facebook_id',$data['facebook_id']);
+            return $this->db->get()->result_array();        
+        }
+        if($data['email']!=null){
+            $this->db->select('a.*, ac.*');
+            $this->db->from('asistente as a');
+            $this->db->join('asistente_carnet as ac','a.id=ac.asistente_id');
+            $this->db->join('carnet as c','c.id=ac.carnet_id');
+            $this->db->where('a.email',$data['email']);
+            return $this->db->get()->result_array(); 
+        }
+        return null;
     }
 }
